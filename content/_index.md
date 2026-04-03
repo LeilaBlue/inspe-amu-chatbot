09937f59b64848e877e7b629d180ce05451aceb0
LeilaBlue <leila.lemmou@gmail.com>
2026-04-03 13:32:10 +0200

    premier commit

---
+++ content/_index.md
@@ -0,0 +1,16 @@
+---
+title: "Accueil"
+---
+
+# Bienvenue sur l'assistant Insp'E-Bot
+
+Ce portail est dédié aux étudiants de l'INSPE Aix-Marseille. Vous y trouverez :
+
+* Les guides pour vos stages.
+* Les informations sur la scolarité.
+* Un chatbot intelligent (en bas à droite) pour répondre à vos questions complexes.
+
+---
+
+*Projet Open Source & Respectueux du RGPD.*
+
---
+++ public/searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "",
+    "content": "Bienvenue sur l’assistant Insp’E-Bot\rCe portail est dédié aux étudiants de l’INSPE Aix-Marseille. Vous y trouverez :\nLes guides pour vos stages. Les informations sur la scolarité. Un chatbot intelligent (en bas à droite) pour répondre à vos questions complexes. Projet Open Source \u0026 Respectueux du RGPD.",
+    "description": "Bienvenue sur l’assistant Insp’E-Bot\rCe portail est dédié aux étudiants de l’INSPE Aix-Marseille. Vous y trouverez :\nLes guides pour vos stages. Les informations sur la scolarité. Un chatbot intelligent (en bas à droite) pour répondre à vos questions complexes. Projet Open Source \u0026 Respectueux du RGPD.",
+    "tags": [],
+    "title": "Accueil",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Accueil",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Accueil",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
