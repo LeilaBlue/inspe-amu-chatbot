2b506e9ec27ac69ef5aecf8aa4c7569f1c33f7cd
LeilaBlue <leila.lemmou@gmail.com>
2026-04-09 16:07:24 +0200

    ajout couleurs INSPE

--- public/searchindex.en.js
+++ public/searchindex.en.js
@@ -1,6 +1,14 @@
 var relearn_searchindex = [
   {
-    "breadcrumb": "Insp'E-Bot vous souhaite la bienvenue",
+    "breadcrumb": "",
+    "content": "Insp’E-Bot est l’assistant virtuel officiel de l’INSPE d’Aix-Marseille Université.\nIl s’adresse à toute personne souhaitant obtenir des informations sur l’INSPE d’Aix-Marseille Université : étudiants, futurs étudiants, enseignants, personnels administratifs ou tout autre public intéressé par l’INSPE et ses activités.\nVous y trouverez :\ndes informations sur les formations proposées par l’INSPE ; des informations sur les admissions et les inscriptions ; des informations sur la scolarité et les stages. Insp’E-Bot est respectueux du RGPD.",
+    "description": "Insp’E-Bot est l’assistant virtuel officiel de l’INSPE d’Aix-Marseille Université.\nIl s’adresse à toute personne souhaitant obtenir des informations sur l’INSPE d’Aix-Marseille Université : étudiants, futurs étudiants, enseignants, personnels administratifs ou tout autre public intéressé par l’INSPE et ses activités.\nVous y trouverez :\ndes informations sur les formations proposées par l’INSPE ; des informations sur les admissions et les inscriptions ; des informations sur la scolarité et les stages. Insp’E-Bot est respectueux du RGPD.",
+    "tags": [],
+    "title": "Bienvenue !",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Bienvenue !",
     "content": "",
     "description": "",
     "tags": [],
@@ -8,15 +16,7 @@ var relearn_searchindex = [
     "uri": "/categories/index.html"
   },
   {
-    "breadcrumb": "",
-    "content": "Insp’E-Bot est l’assistant virtuel officiel de l’INSPE d’Aix-Marseille Université.\nIl s’adresse à toute personne souhaitant obtenir des informations sur l’INSPE d’Aix-Marseille Université : étudiants, futurs étudiants, enseignants, personnels administratifs ou tout autre public intéressé par l’INSPE et ses activités.\nVous y trouverez :\ndes informations sur les formations proposées par l’INSPE ; des informations sur les admissions et les inscriptions ; des informations sur la scolarité et les stages. Insp’E-Bot est respectueux du RGPD.",
-    "description": "Insp’E-Bot est l’assistant virtuel officiel de l’INSPE d’Aix-Marseille Université.\nIl s’adresse à toute personne souhaitant obtenir des informations sur l’INSPE d’Aix-Marseille Université : étudiants, futurs étudiants, enseignants, personnels administratifs ou tout autre public intéressé par l’INSPE et ses activités.\nVous y trouverez :\ndes informations sur les formations proposées par l’INSPE ; des informations sur les admissions et les inscriptions ; des informations sur la scolarité et les stages. Insp’E-Bot est respectueux du RGPD.",
-    "tags": [],
-    "title": "Insp'E-Bot vous souhaite la bienvenue",
-    "uri": "/index.html"
-  },
-  {
-    "breadcrumb": "Insp'E-Bot vous souhaite la bienvenue",
+    "breadcrumb": "Bienvenue !",
     "content": "",
     "description": "",
     "tags": [],
